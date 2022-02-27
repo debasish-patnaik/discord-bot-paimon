3e0acd513ed066c62eb0c2d817a53ae491d21b11
Sudhanshu <perfectsudh@gmail.com>
2022-02-27 12:29:46 +0530

    Set bot presence activities

--- src/events/onReady.ts
+++ src/events/onReady.ts
@@ -5,6 +5,16 @@ import { Client } from "discord.js"
 import { CommandList } from "../commands/_CommandList"
 
 export async function onReady(BOT: Client) {
+
+  BOT.user?.setPresence({
+    activities: [
+      { name: "Dance with SahilRider", type: "STREAMING" },
+      { name: "Character builds with Rakiyun", type: "WATCHING" },
+      { name: "in trials with Prayas", type: "COMPETING" },
+      { name: "Not an emergency food!" }
+    ],
+  })
+
   const rest = new REST({ version: "9" }).setToken(
     process.env.BOT_TOKEN,
   )
