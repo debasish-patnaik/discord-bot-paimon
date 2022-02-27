e42802a915fcb7fd19257760c8b7937e50b1d95a
Sudhanshu <perfectsudh@gmail.com>
2022-02-27 18:37:58 +0530

    Add health checking route

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Client } from "discord.js"
+import fastify from "fastify"
 
 import { IntentOptions } from "./config/IntentOptions"
 import { onInteraction } from "./events/onInteraction"
@@ -15,4 +16,22 @@ import { onReady } from "./events/onReady"
   )
 
   await BOT.login(process.env.BOT_TOKEN)
+
+
+  // define a route for health checking
+  const server = fastify({ logger: true })
+  server.get("/", async () => {
+    return { name: "Paimon", description: "Traveller's best friend" }
+  })
+
+  const start = async () => {
+    try {
+      await server.listen(8080, "0.0.0.0")
+    } catch (err) {
+      server.log.error(err)
+      process.exit(1)
+    }
+  }
+  start()
+
 })()
