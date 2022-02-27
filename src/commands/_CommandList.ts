d48be2f5a90b171f16f8b291b71829ec2ba6def1
Sudhanshu <perfectsudh@gmail.com>
2022-02-27 12:17:12 +0530

    Add simps command

--- src/commands/_CommandList.ts
+++ src/commands/_CommandList.ts
@@ -1,4 +1,5 @@
 import { ICommand } from "../interfaces/command"
+import { assemble } from "./assemble"
 import { ping } from "./ping"
 
-export const CommandList: ICommand[] = [ping]
+export const CommandList: ICommand[] = [ping, assemble]
---
+++ src/commands/assemble.ts
@@ -0,0 +1,30 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { CommandInteraction } from "discord.js";
+
+import { ICommand } from "../interfaces/command";
+
+export const assemble: ICommand = {
+  data: new SlashCommandBuilder()
+    .setName("simps")
+    .setDescription("Simps! Assemble!"),
+  run: async (interaction: CommandInteraction) => {
+    const guild = interaction.guild
+    let message
+    const mentionedRoles: string[] = []
+    if (guild) {
+      const simpsRole = guild.roles.cache.find(role => role.name === "simps")
+      if (simpsRole) {
+        message = `<@&${simpsRole?.id}> Assemble!`
+        mentionedRoles.push(simpsRole.id)
+      }
+    } else {
+      message = "Assemble!"
+    }
+    message = `${message}\nhttps://tenor.com/boIPu.gif`
+
+    await interaction.reply({
+      content: message,
+      allowedMentions: { roles: mentionedRoles },
+    })
+  }
+}
